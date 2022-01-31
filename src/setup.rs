3f5a1a7ab0afa81aa3426b73028978cdfb7db79b
Andre Julius <noromoron@gmail.com>
2022-01-31 01:30:46 +0100

    Move loop into own function

--- src/setup.rs
+++ src/setup.rs
@@ -14,6 +14,7 @@ use poise::{
 };
 use rs621::client::Client;
 
+#[derive(Clone, Debug)]
 pub struct Data {
     /// map of guilds and their channel which to send pokemon to
     channels: Arc<RwLock<HashMap<GuildId, ChannelId>>>,
@@ -63,90 +64,91 @@ pub async fn setup<U, E>(
     _framework: &Framework<U, E>,
 ) -> Result<crate::Data, crate::Error> {
     let data = Data::new();
-    let channels = data.channels();
+    let poke_data = data.clone();
 
     let discord_http = context.http.clone();
 
-    tokio::spawn(async move {
-        println!("Waiting 15 seconds before first run.");
-        tokio::time::sleep(Duration::from_secs(15)).await;
-
-        let e6client = Client::new("https://e926.net", "CutePokebot/0.1.0 (norom)").unwrap();
-        loop {
-            {
-                let post = e6client
-                    .search_random_post(
-                        &[
-                            "pokémon_(species)",
-                            "-vore",
-                            "-gore",
-                            "-transformation",
-                            "-pokémorph",
-                            "-comic",
-                            "-pregnant",
-                            "-foot_focus",
-                            "-seductive",
-                            "score:>55",
-                        ][..],
-                    )
-                    .await;
-
-                match post {
-                    Err(err) => {
-                        let channel = UserId(160518747713437696)
-                            .create_dm_channel(&discord_http)
-                            .await
-                            .unwrap();
-                        let _ = channel
-                            .say(&discord_http, format!("Error: ```{:#?}```", err))
-                            .await;
-                    }
-                    Ok(post) => {
-                        println!("Posting {:?}", post.id);
-                        let channels = channels.read().await.clone();
-                        let discord_http = discord_http.clone();
-
-                        tokio::spawn(async move {
-                            let embed = CreateEmbed::default()
-                                .colour(0x203f6c_u32)
-                                .title(format!("#{}", post.id))
-                                .description(post.description)
-                                .url(&post.file.url.as_ref().unwrap())
-                                .image(post.file.url.unwrap())
-                                .field(
-                                    "Artist(s)",
-                                    format!("{}", post.tags.artist.join(", ")),
-                                    false,
-                                )
-                                .footer(|footer| {
-                                    let score = format!(
-                                        "up: {}, down: {}, total: {}",
-                                        post.score.up,
-                                        post.score.down,
-                                        post.score.up + post.score.down,
-                                    );
-                                    footer.text(score)
-                                })
-                                .to_owned();
-
-                            let mut channel_futures = Vec::with_capacity(channels.len());
-
-                            for channel in channels.values() {
-                                let fut = channel.send_message(discord_http.clone(), |f| {
-                                    f.set_embed(embed.clone())
-                                });
-                                channel_futures.push(fut);
-                            }
-
-                            join_all(channel_futures).await;
-                        });
-                    }
-                }
-            }
-            // 20 * 60s = 1200s
-            tokio::time::sleep(Duration::from_secs(1200)).await;
-        }
-    });
+    tokio::spawn(async move { poke_loop(discord_http, poke_data).await });
 
     Ok(data)
+}
+
+async fn poke_loop(discord_http: Arc<poise::serenity_prelude::Http>, data: Data) -> ! {
+    let channels: Arc<RwLock<HashMap<GuildId, ChannelId>>> = data.channels;
+    println!("Waiting 15 seconds before first run.");
+    tokio::time::sleep(Duration::from_secs(15)).await;
+    let e6client = Client::new("https://e926.net", "CutePokebot/0.1.0 (norom)").unwrap();
+    loop {
+        {
+            let post = e6client
+                .search_random_post(
+                    &[
+                        "pokémon_(species)",
+                        "-vore",
+                        "-gore",
+                        "-transformation",
+                        "-pokémorph",
+                        "-comic",
+                        "-pregnant",
+                        "-foot_focus",
+                        "-seductive",
+                        "score:>55",
+                    ][..],
+                )
+                .await;
+
+            match post {
+                Err(err) => {
+                    let channel = UserId(160518747713437696)
+                        .create_dm_channel(&discord_http)
+                        .await
+                        .unwrap();
+                    let _ = channel
+                        .say(&discord_http, format!("Error: ```{:#?}```", err))
+                        .await;
+                }
+                Ok(post) => {
+                    println!("Posting {:?}", post.id);
+                    let channels = channels.read().await.clone();
+                    let discord_http = discord_http.clone();
+
+                    tokio::spawn(async move {
+                        let embed = CreateEmbed::default()
+                            .colour(0x203f6c_u32)
+                            .title(format!("#{}", post.id))
+                            .description(post.description)
+                            .url(&post.file.url.as_ref().unwrap())
+                            .image(post.file.url.unwrap())
+                            .field(
+                                "Artist(s)",
+                                format!("{}", post.tags.artist.join(", ")),
+                                false,
+                            )
+                            .footer(|footer| {
+                                let score = format!(
+                                    "up: {}, down: {}, total: {}",
+                                    post.score.up,
+                                    post.score.down,
+                                    post.score.up + post.score.down,
+                                );
+                                footer.text(score)
+                            })
+                            .to_owned();
+
+                        let mut channel_futures = Vec::with_capacity(channels.len());
+
+                        for channel in channels.values() {
+                            let fut = channel
+                                .send_message(discord_http.clone(), |f| f.set_embed(embed.clone()));
+                            channel_futures.push(fut);
+                        }
+
+                        join_all(channel_futures).await;
+                    });
+                }
+            }
+        }
+        // 20 * 60s = 1200s
+        tokio::time::sleep(Duration::from_secs(1200)).await;
+    }
 }
