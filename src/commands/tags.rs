e92eaff8e66a06236f547fe173b9a2b1765c8070
Andre <noromoron@gmail.com>
2022-01-31 20:00:37 +0100

    Modify tags command to edit current tags

--- src/commands/tags.rs
+++ src/commands/tags.rs
@@ -4,11 +4,28 @@ use crate::{Context, Error};
 
 /// Gets or sets the timeout
 #[poise::command(prefix_command, slash_command, owners_only)]
-pub async fn tags(ctx: Context<'_>) -> Result<(), Error> {
+pub async fn tags(
+    ctx: Context<'_>,
+    #[description = "If provided, will set these as the new tags"] tags: Option<String>,
+) -> Result<(), Error> {
     let current_tags_lock = ctx.data().tags();
-    let current_tags = current_tags_lock.read().await;
+    let content = if let Some(new_tags) = tags {
+        let mut current_tags = current_tags_lock.write().await;
+        let content = format!(
+            "Old tags: {}\nNew tags: {}",
+            current_tags.join(" "),
+            new_tags
+        );
 
-    let content = current_tags.join(", ");
+        *current_tags = new_tags
+            .split_ascii_whitespace()
+            .map(|s| s.to_string())
+            .collect();
+        content
+    } else {
+        let current_tags = current_tags_lock.read().await;
+        current_tags.join(" ")
+    };
 
     send_reply(ctx, |f| f.content(content).ephemeral(true)).await?;
 
