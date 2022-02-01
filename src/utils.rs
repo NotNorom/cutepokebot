d92f8277ab97b0748c80832b2ec3c6dc32f042a1
Andre <noromoron@gmail.com>
2022-02-01 01:43:39 +0100

    Fix clippy warnings

--- src/utils.rs
+++ src/utils.rs
@@ -6,25 +6,9 @@ pub fn embed_from_post(post: &Post) -> Result<CreateEmbed, String> {
         .colour(0x203f6c_u32)
         .title(format!("#{}", post.id))
         .description(&post.description)
-        .url(
-            &post
-                .file
-                .url
-                .as_ref()
-                .ok_or_else(|| "No url on post object")?,
-        )
-        .image(
-            &post
-                .file
-                .url
-                .as_ref()
-                .ok_or_else(|| "No url on post object")?,
-        )
-        .field(
-            "Artist(s)",
-            format!("{}", post.tags.artist.join(", ")),
-            false,
-        )
+        .url(&post.file.url.as_ref().ok_or("No url on post object")?)
+        .image(&post.file.url.as_ref().ok_or("No url on post object")?)
+        .field("Artist(s)", post.tags.artist.join(", "), false)
         .footer(|footer| {
             let score = format!(
                 "up: {}, down: {}, total: {}",
