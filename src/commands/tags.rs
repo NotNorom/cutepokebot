5aecb78534f950b65f7c38b1c11aa24eedef9c18
Andre <noromoron@gmail.com>
2022-02-03 01:25:57 +0100

    Add getter/setter for tags

--- src/commands/tags.rs
+++ src/commands/tags.rs
@@ -1,4 +1,4 @@
-use poise::send_reply;
+use poise::{send_reply, serenity_prelude::ChannelId};
 
 use crate::{Context, Error};
 
@@ -6,25 +6,39 @@ use crate::{Context, Error};
 #[poise::command(prefix_command, slash_command, owners_only)]
 pub async fn tags(
     ctx: Context<'_>,
+    #[description = "Selected channel"] channel: Option<ChannelId>,
     #[description = "If provided, will set these as the new tags"] tags: Option<String>,
 ) -> Result<(), Error> {
-    let current_tags_lock = ctx.data().tags();
-    let content = if let Some(new_tags) = tags {
-        let mut current_tags = current_tags_lock.write().await;
-        let content = format!(
-            "Old tags: {}\nNew tags: {}",
-            current_tags.join(" "),
-            new_tags
-        );
+    let guild = ctx.guild_id().ok_or("Command must be run in server")?;
+    let channel = channel.unwrap_or_else(|| ctx.channel_id());
 
-        *current_tags = new_tags
+    let current_tags = ctx.data().tags(guild, channel).await;
+
+    let content = if let Some(new_tags) = tags {
+        let new_tags: Vec<String> = new_tags
             .split_ascii_whitespace()
             .map(|s| s.to_string())
             .collect();
+
+        let content = if let Some(current_tags) = current_tags {
+            format!(
+                "Old tags: {}\nNew tags: {}",
+                current_tags.join(" "),
+                &new_tags.join(" ")
+            )
+        } else {
+            format!("Old tags are not set.\nNew tags: {}", new_tags.join(" "))
+        };
+
+        ctx.data().set_tags(guild, channel, new_tags).await;
+
         content
     } else {
-        let current_tags = current_tags_lock.read().await;
-        current_tags.join(" ")
+        if let Some(current_tags) = current_tags {
+            current_tags.join(" ")
+        } else {
+            "Tags are not set.\n".to_string()
+        }
     };
 
     send_reply(ctx, |f| f.content(content).ephemeral(true)).await?;
--- src/setup.rs
+++ src/setup.rs
@@ -21,23 +21,18 @@ pub struct Data {
     guild_configurations: Arc<RwLock<HashMap<GuildId, GuildConfiguration>>>,
     /// timeout in minutes
     timeout: Arc<AtomicU64>,
-    /// the tags which to search for
-    tags: Arc<RwLock<Vec<String>>>,
+
+    
 }
 
 impl Data {
-    fn new<T, S>(tags: T) -> Self
-    where
-        T: IntoIterator<Item = S>,
-        S: Into<String>,
-    {
-        let tags = tags.into_iter().map(|s| s.into()).collect();
+    fn new() -> Self {
         Self {
             channels: Arc::new(RwLock::new(HashMap::new())),
             guild_configurations: Arc::new(RwLock::new(HashMap::new())),
             // 40 minutes as the default timeout
             timeout: Arc::new(AtomicU64::new(40)),
-            tags: Arc::new(RwLock::new(tags)),
+            
         }
     }
 
@@ -68,9 +63,19 @@ impl Data {
         self.timeout.store(timeout, Ordering::Relaxed);
     }
 
-    /// Get an arc to the data's tags.
-    pub fn tags(&self) -> Arc<RwLock<Vec<String>>> {
-        self.tags.clone()
+    /// Get the tags for a channel in a guild
+    pub async fn tags(&self, guild: GuildId, channel: ChannelId) -> Option<Vec<String>> {
+        let conf = self.guild_configurations.read().await;
+        conf.get(&guild)
+            .map(|c| c.tags(&channel).cloned())
+            .flatten()
+    }
+
+    /// Set the tags for a channel in a guild
+    pub async fn set_tags(&self, guild: GuildId, channel: ChannelId, tags: Vec<String>) {
+        let mut conf = self.guild_configurations.write().await;
+        conf.entry(guild).and_modify(|c| c.set_tags(channel, tags));
+    }
     }
 }
 
@@ -79,36 +84,9 @@ pub async fn setup<U, E>(
     _ready: &Ready,
     _framework: &Framework<U, E>,
 ) -> Result<crate::Data, crate::Error> {
-    let data = Data::new([
-        "pokémon_(species)",
-        "-abs",
-        "-blob(disambiguation)",
-        "-breasts",
-        "-butt",
-        "-card_game",
-        "-comic",
-        "-diaper",
-        "-dominatrix",
-        "-expansion",
-        "-foot_focus",
-        "-gore",
-        "-human",
-        "-inflation",
-        "-model_sheet",
-        "-muscular",
-        "-nightmare_fuel",
-        "-nipples",
-        "-overweight",
-        "-pokémorph",
-        "-pregnant",
-        "-seductive",
-        "-thick_thighs",
-        "-transformation",
-        "-vore",
-        "score:>55",
-    ]);
+    let data = Data::new();
 
-    tokio::spawn(poke_loop(context.http.clone(), data.clone()));
+    tokio::spawn(poke_loop(context.clone(), data.clone()));
 
     Ok(data)
 }
